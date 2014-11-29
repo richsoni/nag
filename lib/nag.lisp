ecd9c061eabe4fcec24e53f4135ea48ae8a8958f
Rich Soni <rich@richsoni.com>
2014-11-28 22:59:07 -0500

    no cheating :)

---
+++ lib/nag.lisp
@@ -0,0 +1,44 @@
+(load "/Users/rich/nag/lib/each-file-line.lisp")
+
+(defun get-date ()
+ (multiple-value-bind (_ _ _ day month year) (get-decoded-time)
+   (format nil "~A-~A-~A" year month day)))
+
+(defun query-text (task)
+  (format nil "~A ~A" (get-date) task))
+
+(defun task-is-pending? (task)
+  (eql
+    1
+    (run-program
+      "grep"
+      :arguments (list (query-text task) "/Users/rich/.nag/data")
+      :output nil
+      )))
+
+(defun query (task)
+  (format t "did you ~A today?(yes no)~%> " task)
+  (catch 'query
+    (let ((answer (read t nil 'exit)))
+          (cond
+            ((eql 'yes answer) t)
+            ((eql 'no answer) nil)
+            (t
+              (format t "~%")
+               (query task)))
+          )))
+
+
+(defun read-lines (path)
+  (setf lines NIL)
+  (each-file-line path
+    #'(lambda (line)
+    (setf lines (append lines (list line)))))
+  lines)
+
+(defun process-task (task)
+  (if (task-is-pending? task)
+    (if (query task)
+      (with-open-file (out "/Users/rich/.nag/data" :direction :output :if-exists :append )
+        (princ (format nil "~A~%" (query-text task)) out))))
+  )
