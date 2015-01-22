764f4f2539664b8d9361aa5ed2ec7107578a9ce4
Rich Soni <rich@richsoni.com>
2015-01-22 18:13:13 -0500

    remove lisp code

--- lispCode/defaults.lisp
+++
@@ -1,7 +0,0 @@
-(setf nag-count 3)
-
-(defun choose-task-subset(src dst)
-  (let ((task (nth (random (length src)) src)))
-    (if (find task dst)
-      (choose-task-subset src dst)
-      (append dst (list task)))))
--- lispCode/each-file-line.lisp
+++
@@ -1,7 +0,0 @@
-(defun each-file-line (path fn)
-  (with-open-file (in path :direction :input)
-    (do ((line (read-line in nil 'eof)
-               (read-line in nil 'eof)))
-      ((eql line 'eof))
-      (funcall fn line))))
-
