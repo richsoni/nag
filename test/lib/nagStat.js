cda10dd702dc6c1438c2d4ab2e397ee0fe6d9884
Rich Soni <rich@richsoni.com>
2015-03-05 18:52:25 -0500

    idk

---
+++ test/lib/nagStat.js
@@ -0,0 +1,10 @@
+require("../globals")
+const NagStat = require("../../lib/nagStat")
+const io      = require("./nagIO")
+const C       = require("../../lib/constants")
+const history = io.loadHistory({})
+
+suite('Stat',(() =>
+  test('initial',(() => {
+}))))
+
