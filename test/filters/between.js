578d233408222df21c78cd3bc9fd3eb475040921
Rich Soni <rich@richsoni.com>
2015-03-06 19:04:33 -0500

    add between test

---
+++ test/filters/between.js
@@ -0,0 +1,30 @@
+const moment   = require("moment")
+const between  = require("../../filters/between")
+require("../globals")
+const FORMAT = 'hh:mma'
+
+const truthySingle = {
+  question: 'test',
+  lastOccurance: moment().subtract(5, 'days'),
+  filters: {
+    between: [
+      moment().subtract(5, 'minutes').format(FORMAT),
+      moment().add(5, 'minutes').format(FORMAT)
+    ]
+  }
+}
+const falsy = {
+  question: 'test',
+  lastOccurance: moment().subtract(5, 'days'),
+  filters: {
+    between: [
+      moment().subtract(5, 'minutes').format(FORMAT),
+      moment().subtract(2, 'minutes').format(FORMAT)
+    ]
+  }
+}
+suite('Filters',(() =>
+  test('Today',(() => {
+    assert(between(truthySingle) == true)
+    assert(between(falsy) == false)
+}))))
